3433c87396df62f9b89bcf690a2062966a0851e9
Hiroyuki Ono <hiono@www4113uj.sakura.ne.jp>
2014-01-04 22:48:21 +0900

    Delete the original file if two file are same file.

--- relink.sh
+++ relink.sh
@@ -3,10 +3,14 @@
 cd $HOME/dotfiles
 git pull
 
-DOT_FILES=(.addpath .aspell.conf .bash_aliases .bashrc .gitconfig .gitignore .profile .tmux.conf .emacs.d/init.el)
+DOT_FILES=(.addpath .aspell.conf .bash_aliases .bashrc .bash_logout .gitconfig .gitignore .profile .tmux.conf .emacs.d/init.el)
 
 for file in ${DOT_FILES[@]}
 do
+    diff -w $HOME/dotfiles/$file $HOME/$file 2>&1 > /dev/null
+    if [ $? == 0 ]; then
+	rm -f  $HOME/$file
+    fi
     if [ -a $HOME/$file ]; then
 	ln -s $HOME/dotfiles/$file $HOME/$file.dot
 	echo "ファイルが存在しますから.dotファイルつくるよ: $file"
