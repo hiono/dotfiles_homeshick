8eec684f6a78f7dc510f870535f955a9084eed44
Hiroyuki Ono <hiono@www4113uj.sakura.ne.jp>
2014-01-04 23:32:08 +0900

    Fix script

--- relink.sh
+++ relink.sh
@@ -8,14 +8,16 @@ DOT_FILES=(.addpath .aspell.conf .bash_aliases .bashrc .bash_logout .gitconfig .
 for file in ${DOT_FILES[@]}
 do
     diff -w $HOME/dotfiles/$file $HOME/$file 2>&1 > /dev/null
-    if [ $? == 0 ]; then
+    if [ $? == 0 ]&&[ ! -L $HOME/$file ]; then
 	rm -f  $HOME/$file
     fi
     if [ -a $HOME/$file ]; then
-	ln -s $HOME/dotfiles/$file $HOME/$file.dot
-	echo "ファイルが存在しますから.dotファイルつくるよ: $file"
+	if [ ! -L $HOME/$file ]; then
+	    ln -s $HOME/dotfiles/$file $HOME/$file.dot && \
+	    echo "ファイルが存在しますから.dotファイルつくるよ: $file"
+	fi
     else
-	ln -s $HOME/dotfiles/$file $HOME/$file
+	ln -s $HOME/dotfiles/$file $HOME/$file && \
 	echo "シンボリックリンクを貼りました: $file"
     fi
 done
