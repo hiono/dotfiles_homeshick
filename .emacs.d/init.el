b9744da027871f28a1a48ab345009279fdf02009
Hiroyuki Ono <hiroyuki.ono.jc@renesas.com>
2014-01-22 14:06:27 +0900

    * .emacs.d/init.el: add helm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -107,6 +107,7 @@
     ag
     wgrep-ag
     smartparens
+    helm
     ))
 
 (let ((not-installed (loop for x in installing-package-list
@@ -243,6 +244,44 @@
 ; Fuco1/smartparens Wiki https://github.com/Fuco1/smartparens/wiki#wiki-information-for-new-users
 (smartparens-global-mode t)
 (require 'smartparens-config)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; helm
+;  おすすめの helm拡張 - Life is very short http://d.hatena.ne.jp/syohex/20121207/1354885367
+(global-set-key (kbd "M-y") `helm-show-kill-ring)
+;; helm-git-project
+(defun helm-c-sources-git-project-for (pwd)
+  (loop for elt in
+	'(("Modified files" . "--modified")
+	  ("Untracked files" . "--others --exclude-standard")
+	  ("All controlled files in this project" . nil))
+	for title  = (format "%s (%s)" (car elt) pwd)
+	for option = (cdr elt)
+	for cmd    = (format "git ls-files %s" (or option ""))
+	collect
+	`((name . ,title)
+	  (init . (lambda ()
+		    (unless (and (not ,option) (helm-candidate-buffer))
+		      (with-current-buffer (helm-candidate-buffer 'global)
+			(call-process-shell-command ,cmd nil t nil)))))
+	  (candidates-in-buffer)
+	  (type . file))))
+
+(defun helm-git-project-topdir ()
+  (file-name-as-directory
+   (replace-regexp-in-string
+    "\n" ""
+    (shell-command-to-string "git rev-parse --show-toplevel"))))
+
+(defun helm-git-project ()
+  (interactive)
+  (let ((topdir (helm-git-project-topdir)))
+    (unless (file-directory-p topdir)
+      (error "I'm not in Git Repository!!"))
+    (let* ((default-directory topdir)
+	   (sources (helm-c-sources-git-project-for default-directory)))
+            (helm-other-buffer sources "*helm git project*"))))
+(global-set-key (kbd "C-h g") `helm-git-project)
 
 ;; fix code ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; debian-wheezy等ではemacs24に問題あるため
