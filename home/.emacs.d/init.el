c63779cf766dc38f0996d299df063b78e73c996b
Hiroyuki Ono <hiroyuki.ono.jc@renesas.com>
2014-04-14 11:06:56 +0900

    add text-adjust

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -87,9 +87,9 @@
 (require 'package)
 ;; Add package-archives
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 ;; Initialize
 (package-initialize)
@@ -190,21 +190,21 @@
 ;; 行末の空白を削除
 (require 'whitespace)
 (setq whitespace-style '(face           ; faceで可視化
-			 trailing       ; 行末
-			 tabs           ; タブ
-			 spaces         ; スペース
-			 empty          ; 先頭/末尾の空行
-			 space-mark     ; 表示のマッピング
-			 tab-mark
-			 ))
+                         trailing       ; 行末
+                         tabs           ; タブ
+                         spaces         ; スペース
+                         empty          ; 先頭/末尾の空行
+                         space-mark     ; 表示のマッピング
+                         tab-mark
+                         ))
 (setq whitespace-display-mappings
       '((space-mark ?\u3000 [?\u25a1])
-	;; WARNING: the mapping below has a problem.
-	;; When a TAB occupies exactly one column, it will display the
-	;; character ?\xBB at that column followed by a TAB which goes to
-	;; the next TAB column.
-	;; If this is a problem for you, please, comment the line below.
-	(tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+        ;; WARNING: the mapping below has a problem.
+        ;; When a TAB occupies exactly one column, it will display the
+        ;; character ?\xBB at that column followed by a TAB which goes to
+        ;; the next TAB column.
+        ;; If this is a problem for you, please, comment the line below.
+        (tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
 ;; スペースは全角のみを可視化
 (setq whitespace-space-regexp "\\(\u3000+\\)")
 ;; 保存前に自動でクリーンアップ
@@ -212,23 +212,41 @@
 (global-whitespace-mode 1)
 (defvar my/bg-color "#232323")
 (set-face-attribute 'whitespace-trailing nil
-		    :background my/bg-color
-		    :foreground "DeepPink"
-		    :underline t)
+                    :background my/bg-color
+                    :foreground "DeepPink"
+                    :underline t)
 (set-face-attribute 'whitespace-tab nil
-		    :background my/bg-color
-		    :foreground "LightSkyBlue"
-		    :underline t)
+                    :background my/bg-color
+                    :foreground "LightSkyBlue"
+                    :underline t)
 (set-face-attribute 'whitespace-space nil
-		    :background my/bg-color
-		    :foreground "GreenYellow"
-		    :weight 'bold)
+                    :background my/bg-color
+                    :foreground "GreenYellow"
+                    :weight 'bold)
 (set-face-attribute 'whitespace-empty nil
-		    :background my/bg-color)
+                    :background my/bg-color)
 
 (add-hook 'markdown-mode-hook
-	  '(lambda ()
-	     (set (make-local-variable 'whitespace-action) nil)))
+          '(lambda ()
+             (set (make-local-variable 'whitespace-action) nil)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; asciidoc mode
+(add-to-list 'auto-mode-alist (cons "\\.asc\\'" 'adoc-mode))
+(add-to-list 'auto-mode-alist (cons "\\.asciidoc\\'" 'adoc-mode))
+(add-hook 'adoc-mode-hook (lambda() (buffer-face-mode t)))
+
+;; (autoload 'doc-mode "doc-mode" nil t)
+;; (add-to-list 'auto-mode-alist '("\\.adoc$" . doc-mode))
+;; (add-to-list 'auto-mode-alist '("\\.asc$" . doc-mode))
+;; (add-hook 'doc-mode-hook
+;;        '(lambda ()
+;;           (turn-on-auto-fill)
+;;           (require 'asciidoc)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; text-adjust mode
+(require 'text-adjust)
 
 ;; fix code ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; debian-wheezy等ではemacs24に問題あるため
