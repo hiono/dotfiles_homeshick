6e115b61fe763b790c416d38d2a19d51210221b2
Hiroyuki Ono <hiroyuki.ono.jc@renesas.com>
2015-08-03 16:40:38 +0900

    Fix skk setting

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -101,6 +101,7 @@
          open-junk-file
          dired-filter ;; diredでディレクトリ・特定の拡張子・ファイル名の正規表現にマッチしたもののみを表示 http://rubikitch.com/2015/04/07/dired-filter/
          number ;; カーソル位置の数字に対して簡単な計算をしたり形式を整えたり http://rubikitch.com/2015/06/21/number/
+         ddskk
          ))
 (when (executable-find "git")
   (setq installing-package-list (append '(magit magit-gitflow gist git-gutter) installing-package-list)))
@@ -318,11 +319,16 @@
        (setq default-input-method "japanese-mozc")
        (define-key global-map (kbd "C-\\") 'toggle-input-method))
       ((require 'skk nil t)
-       (global-set-key (kbd "C-x j") 'skk-auto-fill-mode) ;;良い感じに改行を自動入力してくれる機能
+       (define-key global-map (kbd "C-\\") 'toggle-input-method)
+       (define-key global-map (kbd "C-x j") 'skk-auto-fill-mode) ;;良い感じに改行を自動入力してくれる機能
        (setq default-input-method "japanese-skk")         ;;emacs上での日本語入力にskkをつかう
-       (require 'skk-study)                               ;;変換学習機能の追加
-       (define-key global-map (kbd "C-\\") 'toggle-input-method))
-      )
+       (add-hook 'skk-load-hook
+                 (lambda ()
+                   (require 'skk-study)                               ;;変換学習機能の追加
+                   (require 'context-skk)
+                   ))
+       (add-to-list 'context-skk-programming-mode 'c++-mode)
+       ))
 
 (message "Loaded")
 (put 'narrow-to-region 'disabled nil)
