1af30b5ce34862a8ed5dd8ff1b030e0279cfa2da
Hiroyuki Ono <hiroyuki.ono.jc@renesas.com>
2015-08-07 12:47:44 +0900

    Delete custom-set-variables

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -354,18 +354,3 @@
        ))
 
 (message "Loaded")
-(put 'narrow-to-region 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (ac-skk ddskk number dired-filter open-junk-file yaml-mode google-c-style markdown-mode git-gutter gist magit-gitflow magit wgrep-ag ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
