90c0696550109b8d3dac0312a520af252ce44b38
Hiroyuki Ono <hiroyuki.ono.jc@renesas.com>
2017-01-25 12:16:58 +0900

    Fix appearance.

--- home/.emacs.d/init/appearance.el
+++ home/.emacs.d/init/appearance.el
@@ -14,30 +14,30 @@
     (header-line                     :background "#3f3f3f" :box nil))
   "User defined face attributes to override default faces or theme faces.")
 
-(defvar zenburn-override-colors-alist
-  `(("zenburn-fg" . ,user-default-fg)
-    ("zenburn-bg" . ,user-default-bg)))
-;; theme
-(setq frame-background-mode 'dark)
-(if (not (and (>= emacs-major-version 24) (>= emacs-minor-version 1)))
-    (el-get-bundle color-theme
-      (color-theme-initialize)
-      (color-theme-dark-laptop)
-      ;; apply user defined faces
-      (dolist (elt user-face-alist)
-        (let ((name (car elt)) (attrs (cdr elt)))
-          (apply #'set-face-attribute `(,name nil ,@attrs)))))
-  (el-get-lock-unlock 'zenburn-theme)
-  (el-get-bundle zenburn-theme
-    :url "http://raw.github.com/bbatsov/zenburn-emacs/master/zenburn-theme.el"
-    (add-to-list 'custom-theme-load-path
-                 (expand-file-name "zenburn-theme" el-get-dir))
-    (load-theme 'zenburn t)
-    ;; apply user defined faces
-    (let* ((class '((class color) (min-colors 89)))
-           (to-spec #'(lambda (elt) `(,(car elt) ((,class ,(cdr elt))))))
-           (faces (mapcar to-spec user-face-alist)))
-      (apply #'custom-theme-set-faces `(user ,@faces)))))
+;; ;; theme
+;; (setq frame-background-mode 'dark)
+;; (defvar zenburn-override-colors-alist
+;;   `(("zenburn-fg" . ,user-default-fg)
+;;     ("zenburn-bg" . ,user-default-bg)))
+;; (if (not (and (>= emacs-major-version 24) (>= emacs-minor-version 1)))
+;;     (el-get-bundle color-theme
+;;       (color-theme-initialize)
+;;       (color-theme-dark-laptop)
+;;       ;; apply user defined faces
+;;       (dolist (elt user-face-alist)
+;;         (let ((name (car elt)) (attrs (cdr elt)))
+;;           (apply #'set-face-attribute `(,name nil ,@attrs)))))
+;;   (el-get-lock-unlock 'zenburn-theme)
+;;   (el-get-bundle zenburn-theme
+;;     :url "http://raw.github.com/bbatsov/zenburn-emacs/master/zenburn-theme.el"
+;;     (add-to-list 'custom-theme-load-path
+;;                  (expand-file-name "zenburn-theme" el-get-dir))
+;;     (load-theme 'zenburn t)
+;;     ;; apply user defined faces
+;;     (let* ((class '((class color) (min-colors 89)))
+;;            (to-spec #'(lambda (elt) `(,(car elt) ((,class ,(cdr elt))))))
+;;            (faces (mapcar to-spec user-face-alist)))
+;;       (apply #'custom-theme-set-faces `(user ,@faces)))))
 
 ;; fix for non-frame mode; set face color explicitly
 (dolist (face '(default mode-line))
@@ -45,49 +45,36 @@
     (set-face-foreground face (plist-get attrs :foreground))
     (set-face-background face (plist-get attrs :background))))
 
-;; use darker comment
-(defun set-comment-color (color)
-  (set-face-foreground 'font-lock-comment-delimiter-face color)
-  (set-face-foreground 'font-lock-comment-face color))
-(defun darken-comment ()
-  (interactive)
-  (set-comment-color "gray32"))
-(defun lighten-comment ()
-  (interactive)
-  (set-comment-color "OrangeRed"))
-(lighten-comment)
-
-;; highlight specific keywords in comments
-(el-get-bundle fic-mode
-  (add-hook 'prog-mode-hook #'fic-mode) ;; Emacs 24
-  (add-hook 'cperl-mode-hook #'fic-mode)
-  (with-eval-after-load-feature 'fic-mode
-    (push "XXX" fic-highlighted-words)
-    (dolist (face '(fic-face fic-author-face))
-      (set-face-foreground face "#d0bf8f")
-      (set-face-background face "gray40"))))
+;; ;; highlight specific keywords in comments
+;; (el-get-bundle fic-mode
+;;   (add-hook 'prog-mode-hook #'fic-mode) ;; Emacs 24
+;;   (add-hook 'cperl-mode-hook #'fic-mode)
+;;   (with-eval-after-load-feature 'fic-mode
+;;     (push "XXX" fic-highlighted-words)
+;;     (dolist (face '(fic-face fic-author-face))
+;;       (set-face-foreground face "#d0bf8f")
+;;       (set-face-background face "gray40"))))
 
 ;; scroll bar
-(el-get-bundle yascroll
-  (global-yascroll-bar-mode))
+(el-get-bundle yascroll (global-yascroll-bar-mode))
 
-;; line-wrap character
-(require 'disp-table)
-(defface wrap-face
-  '((((class color) (min-colors 88) (background dark))
-     :foreground "aquamarine4")
-    (((class color) (min-colors 88) (background light))
-     :foreground "aquamarine2")
-    (((class color) (min-colors 16))
-     :foreground "DarkCyan")
-    (((class color) (min-colors 8))
-     :foreground "gray")
-    (((type tty) (class mono))
-     :inverse-video t))
-  "Face of the wrap."
-  :group 'convenience)
-(set-display-table-slot standard-display-table 'wrap
-                        (make-glyph-code #xbb 'wrap-face))
+;; ;; line-wrap character
+;; (require 'disp-table)
+;; (defface wrap-face
+;;   '((((class color) (min-colors 88) (background dark))
+;;      :foreground "aquamarine4")
+;;     (((class color) (min-colors 88) (background light))
+;;      :foreground "aquamarine2")
+;;     (((class color) (min-colors 16))
+;;      :foreground "DarkCyan")
+;;     (((class color) (min-colors 8))
+;;      :foreground "gray")
+;;     (((type tty) (class mono))
+;;      :inverse-video t))
+;;   "Face of the wrap."
+;;   :group 'convenience)
+;; (set-display-table-slot standard-display-table 'wrap
+;;                         (make-glyph-code #xbb 'wrap-face))
 
 ;; visualize whitespace
 (with-eval-after-load-feature 'whitespace
@@ -127,13 +114,13 @@
 ;; parenthesis
 (show-paren-mode t)
 
-;; hide-show
-(defun hs-enable ()
-  (interactive)
-  (hs-minor-mode)
-  (local-set-key (kbd "C-c h") 'hs-hide-block)
-  (local-set-key (kbd "C-c s") 'hs-show-block)
-  (local-set-key (kbd "C-c l") 'hs-hide-level))
+;; ;; hide-show
+;; (defun hs-enable ()
+;;   (interactive)
+;;   (hs-minor-mode)
+;;   (local-set-key (kbd "C-c h") 'hs-hide-block)
+;;   (local-set-key (kbd "C-c s") 'hs-show-block)
+;;   (local-set-key (kbd "C-c l") 'hs-hide-level))
 
 ;; set mode-line-inactive face
 (set-face-attribute 'mode-line-inactive nil :background "gray10" :foreground "snow4")
