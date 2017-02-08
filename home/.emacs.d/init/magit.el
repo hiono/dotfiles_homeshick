9c2178b29dd0e9225c6ce6cb0bfda49219da125b
Hiroyuki Ono <hiroyuki.ono.jc@renesas.com>
2017-02-08 12:03:56 +0900

    Fix magit face.

--- home/.emacs.d/init/magit.el
+++ home/.emacs.d/init/magit.el
@@ -12,5 +12,23 @@
       (define-key map (kbd "SPC") 'magit-jump-to-diffstat-or-diff)))
   (with-eval-after-load-feature 'magit-section
     (add-hook 'magit-section-movement-hook 'magit-status-maybe-update-blob-buffer))
-  (with-eval-after-load-feature (magit-mode)
-    (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)))
+  (with-eval-after-load-feature 'magit-mode
+    (add-hook 'magit-mode-hook 'turn-on-magit-gitflow))
+  (with-eval-after-load-feature 'magit-diff
+    ;; cursorが載っていない状態でのaddedのface
+    (set-face-foreground 'magit-diff-added "#00FF00")
+    (set-face-background 'magit-diff-added "#000000")
+    ;; cursorが載っている状態のaddedのface
+    (set-face-foreground 'magit-diff-added-highlight "#00FF00")
+    (set-face-background 'magit-diff-added-highlight "gray20")
+    ;; cursor載っていない状態のremoved
+    (set-face-foreground 'magit-diff-removed "#FF0000")
+    (set-face-background 'magit-diff-removed "#000000")
+    ;; cursor載っている状態のremoved
+    (set-face-foreground 'magit-diff-removed-highlight "#FF0000")
+    (set-face-background 'magit-diff-removed-highlight "gray20")
+    ;; lineを選択してstageしようとするときのface
+    (set-face-background 'magit-diff-lines-boundary "blue")
+    ;;(set-face-foreground 'magit-hash "red")
+    )
+)
