106b4adf9de2b59f270f135c863b57233e228bbc
Hiroyuki Ono <hiroyuki.ono.jc@renesas.com>
2017-02-16 12:03:57 +0900

    Fix keychain config for fish.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,6 @@
 if status --is-interactive
   set -l IFS # this temporarily clears IFS, which disables the newline-splitting
-  eval (keychain --eval --quiet -Q id_rsa)
+  eval (keychain --eval --quiet -Q id_rsa | sed -e 's/\(.*\)=\(.*\); export .*$/set -x \1 \2/g')
 end
 set -x COMPOSE_HTTP_TIMEOUT 300
 if status --is-login
