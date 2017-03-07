cce0d653a261afcd04984a561c8f86ec9f69e867
Hiroyuki Ono <hiroyuki.ono.jc@renesas.com>
2017-03-07 14:48:04 +0900

    Add fish setting for rtags

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,7 +2,16 @@ if status --is-interactive
   set -l IFS # this temporarily clears IFS, which disables the newline-splitting
   eval (keychain --eval --quiet -Q id_rsa | sed -e 's/\(.*\)=\(.*\); export .*$/set -x \1 \2/g')
 end
-set -x COMPOSE_HTTP_TIMEOUT 300
 if status --is-login
+    # for rtags
+    which rdm > /dev/null
+    if test $status -eq 0
+        pgrep rdm > /dev/null
+        if test $status -ne 0
+            rmd --daemon
+        end
+    end
+    # add PATH
     set -x PATH $PATH ~/bin
 end
+set -x COMPOSE_HTTP_TIMEOUT 300
