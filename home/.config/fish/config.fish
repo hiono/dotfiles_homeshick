59512c3719842d421b7c608e2d6327bb2d479eb4
Hiroyuki Ono <hiroyuki.ono.jc@renesas.com>
2017-09-15 14:45:43 +0900

    Add fish setting for ssh

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,10 +1,10 @@
 if status --is-interactive
     set -l IFS # this temporarily clears IFS, which disables the newline-splitting
-    eval (ssh-agent -c) > /dev/null
+    # eval (ssh-agent -c) > /dev/null
 end
 if status --is-login
     # add PATH
-    set -x PATH $PATH ~/bin
+    # set -x PATH $PATH ~/bin
     # set -x PATH $PATH ~/bin ~/.emacs.d/**/el-get/rtags/bin
     # # for rtags
     # which rdm > /dev/null
@@ -15,5 +15,8 @@ if status --is-login
     #     end
     # end
     set -gx PATH $PATH ~/bin
+    # ssh_agent_start
+    # source ~/.config/fish/functions/ssh_agent_start.fish
+    ssh_agent_start
 end
 set -x COMPOSE_HTTP_TIMEOUT 300
